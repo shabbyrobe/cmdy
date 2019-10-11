d084db3320e90551dbfa2d498ac8080fea26339c
shabbyrobe <code@shabbyrobe.org>
2019-10-11 21:53:54 +1100

    Unroll states a bit

--- internal/cmdstr/cmdstr.go
+++ internal/cmdstr/cmdstr.go
@@ -1,30 +1,50 @@
 package cmdstr
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"unsafe"
+)
 
 type stateType int
 
+func (s stateType) String() string {
+	switch s {
+	case stateStart:
+		return "start"
+	case stateSingle:
+		return "squot"
+	case stateDouble:
+		return "dquot"
+	case stateBare:
+		return "arg"
+	case stateBareSingle:
+		return "squot"
+	case stateBareDouble:
+		return "dquot"
+	case stateEsc:
+		return "esc"
+	default:
+		panic("unknown state type")
+	}
+}
+
 const (
-	stateStart stateType = iota + 1
-	stateArg
+	stateStart stateType = iota
+	stateSingle
+	stateDouble
+	stateBare
+	stateBareSingle
+	stateBareDouble
 	stateEsc
 )
 
-type argType int
-
-const (
-	argNone argType = iota + 1
-	argSingle
-	argDouble
-	argUnquot
-)
-
-var (
-	ErrUnterminatedEscape = errors.New("cmdstr: unterminated escape")
-	ErrUnterminatedQuote  = errors.New("cmdstr: unterminated quote")
-)
-
-// SplitString implements shell-esque splitting rules, similar to Python's shlex.
+// Pare implements shell-esque string-splitting rules, similar to Python's shlex.
+//
+// Parsing ends when all arguments are successfully consumed, when invalid syntax
+// is encountered, or when one of the characters in 'endset' is encountered while
+// scanning for the start of the next argument.
 //
 // The input string is stripped of leading and trailing whitespace. Whitespace is defined
 // as '\r', '\n', '\t' and ' '.
@@ -37,75 +57,175 @@ var (
 // Quote characters are not recognized within words; `Do"Not"Separate` is parsed as the
 // single word `Do"Not"Separate`.
 //
-func SplitString(cmd string) ([]string, error) {
+// Enclosing characters in single-quotes ( '' ) shall preserve the literal value of each
+// character within the single-quotes. A single-quote cannot occur within single-quotes.
+//
+// Enclosing characters in double-quotes ( "" ) shall preserve the literal value of all
+// characters within the double-quotes, with the exception of <backslash>, explained
+// below.
+//
+// The <backslash> shall retain its special meaning as an escape character when
+// present in the following two-byte sequences, producing the output on the right
+// side:
+//
+//    0x5c 0x6e (\n)  --> 0x0a
+//    0x5c 0x0a (\\n) --> <nothing>
+//    0x5c 0x22 (\")  --> 0x22
+//    0x5c 0x5c (\\)  --> 0x5c
+//
+// All other backslashes are copied literally into the output:
+//
+//    0x5c 0x61 (\a)  --> 0x5c 0x61
+//
+func Parse(cmd []byte, endset string) (out []string, n int, rerr error) {
 	var (
-		state   = stateStart
-		arg     = argNone
-		current = ""
-		l       = len(cmd)
-		out     []string
+		cur      string
+		sz       = len(cmd)
+		wsp      = [256]bool{'\r': true, '\n': true, '\t': true, ' ': true}
+		esc      = [256]byte{'n': '\n', '"': '"', '\\': '\\'}
+		end      = [256]bool{}
+		stack    = [4]stateType{}
+		stackPos = 0
+		start    = 0
+		i        = 0
 	)
 
-	var wsp = [256]bool{'\r': true, '\n': true, '\t': true, ' ': true}
+	for i = 0; i < len(endset); i++ {
+		end[endset[i]] = true
+	}
 
-	for i := 0; i < l; i++ {
-	retry:
+	for i = 0; i < sz; i++ {
 		c := cmd[i]
-		switch state {
+
+	retry:
+		switch stack[stackPos] {
 		case stateStart:
 			if wsp[c] {
 				continue
+			} else if end[c] {
+				sz = i // break!
+				goto end
 			}
+
 			switch c {
 			case '"':
-				state, arg = stateArg, argDouble
+				stackPos++
+				stack[stackPos], start = stateDouble, i+1
 			case '\'':
-				state, arg = stateArg, argSingle
+				stackPos++
+				stack[stackPos], start = stateSingle, i+1
 			default:
-				state, arg = stateArg, argUnquot
+				stackPos++
+				stack[stackPos], start = stateBare, i
 				goto retry
 			}
 
-		case stateArg:
+		case stateDouble:
 			if c == '\\' {
-				state = stateEsc
+				stackPos++
+				stack[stackPos] = stateEsc
+			} else if c == '"' {
+				cur += string(cmd[start:i])
+				out, cur = append(out, cur), ""
+				stackPos--
+			}
 
-			} else if len(current) > 0 &&
-				(arg == argSingle && c == '\'') ||
-				(arg == argDouble && c == '"') ||
-				(arg == argUnquot && wsp[c]) {
+		case stateSingle:
+			if c == '\\' {
+				stackPos++
+				stack[stackPos] = stateEsc
+			} else if c == '\'' {
+				cur += string(cmd[start:i])
+				out, cur = append(out, cur), ""
+				stackPos--
+			}
 
-				current, out = "", append(out, current)
-				state, arg = stateStart, argNone
+		case stateBare:
+			if c == '\\' {
+				stackPos++
+				stack[stackPos] = stateEsc
+			} else if c == '\'' {
+				stackPos++
+				stack[stackPos] = stateBareSingle
+			} else if c == '"' {
+				stackPos++
+				stack[stackPos] = stateBareDouble
+			} else if wsp[c] {
+				cur += string(cmd[start:i])
+				out, cur = append(out, cur), ""
+				stackPos--
+			}
 
-			} else {
-				current += string(c)
+		case stateBareDouble:
+			if c == '\\' {
+				stackPos++
+				stack[stackPos] = stateEsc
+			} else if c == '"' {
+				cur += string(cmd[start:i])
+				start = i
+				stackPos--
+			}
+
+		case stateBareSingle:
+			if c == '\\' {
+				stackPos++
+				stack[stackPos] = stateEsc
+			} else if c == '\'' {
+				cur += string(cmd[start:i])
+				start = i
+				stackPos--
 			}
 
 		case stateEsc:
-			esc := c == '\n' ||
-				(arg == argSingle && (c == '\'')) ||
-				(arg == argDouble && (c == '"' || c == '\\')) ||
-				(arg == argUnquot && (c == '\'' || c == '"' || c == '\\'))
+			if c == '\n' {
+				// Escaped newlines consume the newline
+				cur += string(cmd[start : i-1])
+				start = i + 1
 
-			if !esc {
-				current += "\\"
+			} else if esc[c] != 0 {
+				cur += string(cmd[start:i-1]) + string(esc[c])
+				start = i + 1
+
+			} else {
+				// backslash is literal, do nothing
 			}
+			stackPos--
 
-			current += string(c)
-			state = stateArg
+		default:
+			panic("unknown state")
 		}
 	}
 
-	if state == stateEsc {
-		return nil, ErrUnterminatedEscape
-	} else if state == stateArg && arg != argUnquot {
-		return nil, ErrUnterminatedQuote
+end:
+	if stack[stackPos] == stateBare {
+		cur += string(cmd[start:sz])
+		out, cur = append(out, cur), ""
+
+	} else if stackPos > 0 {
+		stackStr := ""
+		for pos := 1; pos <= stackPos; pos++ {
+			if pos > 1 {
+				stackStr += " > "
+			}
+			stackStr += stack[pos].String()
+		}
+		return nil, i, fmt.Errorf("cmdstr: %w, stack: %v", ErrIncompleteCommand, stackStr)
 	}
 
-	if len(current) > 0 {
-		out = append(out, current)
-	}
-
-	return out, nil
+	return out, i, nil
 }
+
+func SplitString(cmd string) ([]string, error) {
+	hdr := *(*reflect.StringHeader)(unsafe.Pointer(&cmd))
+	cmdBytes := *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: hdr.Data,
+		Len:  hdr.Len,
+		Cap:  hdr.Len,
+	}))
+	out, _, err := Parse(cmdBytes, "")
+	return out, err
+}
+
+var (
+	ErrIncompleteCommand = errors.New("incomplete command")
+)
--- internal/cmdstr/cmdstr_test.go
+++ internal/cmdstr/cmdstr_test.go
@@ -1,11 +1,15 @@
 package cmdstr
 
 import (
+	"errors"
 	"reflect"
+	"strings"
 	"testing"
+
+	"github.com/shabbyrobe/cmdy/internal/assert"
 )
 
-func assertYep(t *testing.T, in string, out ...string) {
+func yep(t *testing.T, in string, out ...string) {
 	t.Helper()
 	result, err := SplitString(in)
 	if err != nil {
@@ -16,13 +20,89 @@ func assertYep(t *testing.T, in string, out ...string) {
 	}
 }
 
-func TestCmdStr(t *testing.T) {
-	assertYep(t, `foo`, `foo`)
+func nup(t *testing.T, in string, expected error) {
+	t.Helper()
+	_, err := SplitString(in)
+	if err == nil {
+		t.Fatal()
+	}
+	if !errors.Is(err, expected) {
+		t.Fatal(err, "!=", expected)
+	}
+}
+
+func TestSplitString(t *testing.T) {
+	yep(t, ``)
+	yep(t, `  `)
+	yep(t, `""`, "")
+	yep(t, ` "" `, "")
+	yep(t, `''`, "")
+	yep(t, ` '' `, "")
+
+	yep(t, `foo`, `foo`)
+	yep(t, `foo bar`, `foo`, `bar`)
+	yep(t, `foo   bar`, `foo`, `bar`)
+	yep(t, `  foo   bar  `, `foo`, `bar`)
+
+	yep(t, `"foo"`, `foo`)
+	yep(t, `" foo "`, ` foo `)
+	yep(t, `"foo" "bar"`, `foo`, `bar`)
+	yep(t, `"foo"   "bar"`, `foo`, `bar`)
+	yep(t, `  "foo"   "bar"  `, `foo`, `bar`)
+
+	yep(t, `'foo'`, `foo`)
+	yep(t, `' foo '`, ` foo `)
+	yep(t, `'foo' 'bar'`, `foo`, `bar`)
+	yep(t, `'foo'   'bar'`, `foo`, `bar`)
+	yep(t, `  'foo'   'bar'  `, `foo`, `bar`)
+
+	// Newline escapes in double quotes:
+	yep(t, `"\`+"\n"+`foo"`, "foo")
+	yep(t, `"f\`+"\n"+`oo"`, "foo")
+	yep(t, `"fo\`+"\n"+`o"`, "foo")
+	yep(t, `"foo\`+"\n"+`"`, "foo")
+
+	yep(t, `'\nfoo'`, "\nfoo")
+	yep(t, `'f\noo'`, "f\noo")
+	yep(t, `'fo\no'`, "fo\no")
+	yep(t, `'foo\n'`, "foo\n")
+
+	yep(t, `"\\foo"`, "\\foo")
+	yep(t, `"f\\oo"`, "f\\oo")
+	yep(t, `"fo\\o"`, "fo\\o")
+	yep(t, `"foo\\"`, "foo\\")
+
+	// Unknown escape is literal:
+	yep(t, `"\s"`, `\s`)
 
 	// intra-unqouted quotes are passed through:
-	assertYep(t, `foo"bar"baz`, `foo"bar"baz`)
-	assertYep(t, `foo'bar'baz`, `foo'bar'baz`)
+	yep(t, `foo"bar"baz`, `foo"bar"baz`)
+	yep(t, `foo'bar'baz`, `foo'bar'baz`)
 
-	// intra-unqouted quotes don't have to be closed:
-	assertYep(t, `foo'barbaz`, `foo'barbaz`)
+	// intra-unqouted quotes have to be closed:
+	nup(t, `foo'barbaz`, ErrIncompleteCommand)
+}
+
+func TestParseIncompleteErr(t *testing.T) {
+	tt := assert.WrapTB(t)
+	in := "foo'abc\\"
+	_, _, err := Parse([]byte(in), "")
+	tt.Assert(strings.Contains(err.Error(), "arg > squot > esc"))
+}
+
+func TestParseEndSet(t *testing.T) {
+	tt := assert.WrapTB(t)
+	in := "foo bar [yep"
+	out, n, err := Parse([]byte(in), "[")
+	tt.MustOK(err)
+	tt.MustEqual("[yep", in[n:])
+	tt.MustEqual([]string{"foo", "bar"}, out)
+}
+
+var BenchSplitResult []string
+
+func BenchmarkSplitString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		BenchSplitResult, _ = SplitString(`foo bar 'baz' "qux" \n "yep" `)
+	}
 }
