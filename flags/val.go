e6340430f2a4d52f04dca62fb9ad72496342f2bf
Blake Williams <code@shabbyrobe.org>
2020-01-18 15:48:09 +1100

    Polish up list flags a bit, add tests

--- flags/val.go
+++ flags/val.go
@@ -9,15 +9,26 @@ import (
 
 var splitPattern = regexp.MustCompile(`\s*,\s*`)
 
+// StringList is a flag.Getter which allows you to accumulate multiple
+// instances of the same flag into a slice of strings.
+//
+// If your flag is set up like so:
+//	var myList StringList
+//	flag.Var(&myList, "yep", "My List")
+//
+// ...then passing "-yep foo -yep bar" will cause myList to equal
+// '[]string{"foo", "bar"}'.
+//
+// StringList does not make sense to use with an arg.ArgSet; use ArgSet.Remaining()
+// instead.
+//
 type StringList []string
 
+func (s StringList) Get() interface{}  { return []string(s) }
 func (s StringList) Strings() []string { return s }
 
-func (s *StringList) String() string {
-	if s == nil {
-		return ""
-	}
-	return strings.Join(*s, ",")
+func (s StringList) String() string {
+	return strings.Join(s, ",")
 }
 
 func (s *StringList) Set(v string) error {
@@ -25,27 +36,48 @@ func (s *StringList) Set(v string) error {
 	return nil
 }
 
+// IntList is a flag.Getter which allows you to accumulate multiple
+// instances of the same flag into a slice of ints
+//
+// If your flag is set up like so:
+//	var myList IntList
+//	flag.Var(&myList, "yep", "My List")
+//
+// ...then passing '-yep 1 -yep 2' will cause myList to equal '[]int{1, 2}'.
+//
+// IntList also supports comma separated values, so passing -yep 1,2,3
+// or -yep "1, 2, 3" will cause myList to equal '[]int{1, 2, 3}'.
+//
+// IntList uses 'strconv.ParseInt(..., 0, 0)' to parse the value; the base is implied by
+// the string's prefix: base 2 for "0b", base 8 for "0" or "0o", base 16 for "0x", and
+// base 10 otherwise. Underscore characters are permitted per the Go integer literal
+// syntax.
+//
+// IntList does not make sense to use with an arg.ArgSet; use ArgSet.RemainingInts()
+// instead.
+//
 type IntList []int
 
-func (s IntList) Ints() []int { return s }
+func (s IntList) Get() interface{} { return []int(s) }
+func (s IntList) Ints() []int      { return s }
 
-func (s *IntList) String() string {
-	if s == nil {
+func (s IntList) String() string {
+	if len(s) == 0 {
 		return ""
 	}
-	var out []string
-	for _, i := range *s {
+	var out = make([]string, 0, len(s))
+	for _, i := range s {
 		out = append(out, fmt.Sprintf("%d", i))
 	}
 	return strings.Join(out, ",")
 }
 
 func (s *IntList) Set(v string) error {
-	for _, part := range splitPattern.Split(v, -1) {
+	for _, part := range splitPattern.Split(strings.TrimSpace(v), -1) {
 		if len(part) == 0 {
 			continue
 		}
-		i, err := strconv.ParseInt(part, 10, 0)
+		i, err := strconv.ParseInt(part, 0, 0)
 		if err != nil {
 			return err
 		}
@@ -54,27 +86,85 @@ func (s *IntList) Set(v string) error {
 	return nil
 }
 
+// Int64List is a flag.Getter which allows you to accumulate multiple
+// instances of the same flag into a slice of int64s.
+//
+// Int64List is identical to IntList except for the int size; see the IntList
+// documentation for more details.
+//
+// Int64List does not make sense to use with an arg.ArgSet; use ArgSet.RemainingInt64s()
+// instead.
+//
 type Int64List []int64
 
+func (s Int64List) Get() []int64    { return []int64(s) }
 func (s Int64List) Int64s() []int64 { return s }
 
-func (s *Int64List) String() string {
-	if s == nil {
+func (s Int64List) String() string {
+	if len(s) == 0 {
 		return ""
 	}
-	var out []string
-	for _, i := range *s {
+	var out = make([]string, 0, len(s))
+	for _, i := range s {
 		out = append(out, fmt.Sprintf("%d", i))
 	}
 	return strings.Join(out, ",")
 }
 
 func (s *Int64List) Set(v string) error {
-	for _, part := range splitPattern.Split(v, -1) {
+	for _, part := range splitPattern.Split(strings.TrimSpace(v), -1) {
 		if len(part) == 0 {
 			continue
 		}
-		i, err := strconv.ParseInt(part, 10, 64)
+		i, err := strconv.ParseInt(part, 0, 64)
+		if err != nil {
+			return err
+		}
+		*s = append(*s, i)
+	}
+	return nil
+}
+
+// Float64List is a flag.Getter which allows you to accumulate multiple
+// instances of the same flag into a slice of float64s
+//
+// If your flag is set up like so:
+//	var myList Float64List
+//	flag.Var(&myList, "yep", "My List")
+//
+// ...then passing '-yep 1.0 -yep 2.0' will cause myList to equal '[]float64{1.0, 2.0}'.
+//
+// Float64List also supports comma separated values, so passing -yep 1.0,2.0,3.0
+// or -yep "1.0, 2.0, 3.0" will cause myList to equal '[]float64{1.0, 2.0, 3.0}'.
+//
+// Float64List uses 'strconv.ParseFloat(..., 64)' to parse the value, and thus permits
+// all syntax described by the Go documentation as valid.
+//
+// Float64List does not make sense to use with an arg.ArgSet; use ArgSet.RemainingFloat64s()
+// instead.
+//
+type Float64List []float64
+
+func (s Float64List) Get() interface{}    { return []float64(s) }
+func (s Float64List) Float64s() []float64 { return s }
+
+func (s Float64List) String() string {
+	if len(s) == 0 {
+		return ""
+	}
+	var out = make([]string, 0, len(s))
+	for _, i := range s {
+		out = append(out, fmt.Sprintf("%f", i))
+	}
+	return strings.Join(out, ",")
+}
+
+func (s *Float64List) Set(v string) error {
+	for _, part := range splitPattern.Split(strings.TrimSpace(v), -1) {
+		if len(part) == 0 {
+			continue
+		}
+		i, err := strconv.ParseFloat(part, 64)
 		if err != nil {
 			return err
 		}
---
+++ flags/val_test.go
@@ -0,0 +1,179 @@
+package flags
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/shabbyrobe/cmdy"
+	"github.com/shabbyrobe/cmdy/internal/assert"
+)
+
+func TestStringList(t *testing.T) {
+	for idx, tc := range []struct {
+		in  []string
+		out []string
+	}{
+		{nil, nil},
+		{[]string{"-s", ""}, []string{""}},
+		{[]string{"-s", "yep"}, []string{"yep"}},
+		{[]string{"-s", "yep", "-s", "yep"}, []string{"yep", "yep"}},
+	} {
+		t.Run(fmt.Sprintf("%d", idx), func(t *testing.T) {
+			tt := assert.WrapTB(t)
+			var v StringList
+			var fs flag.FlagSet
+			fs.Var(&v, "s", "test")
+			tt.MustOK(fs.Parse(tc.in))
+			tt.MustEqual(StringList(tc.out), v)
+		})
+	}
+}
+
+type numCase struct {
+	ok  bool
+	in  []string
+	out []string
+}
+
+func (sic numCase) AsInts() (vs []int, skip bool) {
+	for _, v := range sic.out {
+		v, err := strconv.ParseInt(v, 0, 0)
+		if err != nil {
+			return nil, true
+		}
+		vs = append(vs, int(v))
+	}
+	return vs, false
+}
+
+func (sic numCase) AsInt64s() (vs []int64, skip bool) {
+	for _, v := range sic.out {
+		v, err := strconv.ParseInt(v, 0, 64)
+		if errors.Is(err, strconv.ErrRange) {
+			return nil, true
+		} else if err != nil {
+			panic(v)
+		}
+		vs = append(vs, v)
+	}
+	return vs, false
+}
+
+func (sic numCase) AsFloat64s() (vs []float64, skip bool) {
+	for _, fv := range sic.out {
+		v, err := strconv.ParseFloat(fv, 64)
+		if errors.Is(err, strconv.ErrRange) {
+			return nil, true
+		} else if err != nil {
+			panic(v)
+		}
+		vs = append(vs, float64(v))
+	}
+	return vs, false
+}
+
+const yep, nup = true, false
+
+// sharedNumCases contains test cases that are usable by all the numeric list
+// flag var tests (floats and ints combined). Avoid underscores as well as hex, binary or
+// octal literals for ints, or decimal portions, exponents, +/-inf, etc for floats.
+//
+// Unary negation is allowed, but will cause a skipped test for unsigned numbers.
+var sharedNumCases = []numCase{
+	{yep, nil, nil},
+	{yep, []string{"-s", ""}, nil},
+	{nup, []string{"-s", "nup"}, nil},
+	{yep, []string{"-s", "1"}, []string{"1"}},
+	{yep, []string{"-s", "1,2,3"}, []string{"1", "2", "3"}},
+	{yep, []string{"-s", "1", "-s", "2", "-s", "3"}, []string{"1", "2", "3"}},
+	{yep, []string{"-s", "1,2", "-s", "3"}, []string{"1", "2", "3"}},
+	{yep, []string{"-s", " 1 , 2 , 3 "}, []string{"1", "2", "3"}},
+}
+
+var sharedIntCases = append(sharedNumCases, []numCase{
+	{yep, []string{"-s", "0xf,0xf,0xf"}, []string{"0xf", "0xf", "0xf"}},
+	{yep, []string{"-s", "0xf_f"}, []string{"0xff"}},
+	{yep, []string{"-s", "0o777"}, []string{"0o777"}},
+}...)
+
+func TestInt64List(t *testing.T) {
+	for idx, tc := range sharedIntCases {
+		t.Run(fmt.Sprintf("int64/%d", idx), func(t *testing.T) {
+			tt := assert.WrapTB(t)
+			out, skip := tc.AsInt64s()
+			if skip {
+				t.Skip()
+			}
+
+			var v Int64List
+			fs := cmdy.NewFlagSet()
+			fs.Var(&v, "s", "test")
+			err := fs.Parse(tc.in)
+
+			if tc.ok {
+				tt.MustOK(err)
+				tt.MustEqual(Int64List(out), v)
+			} else {
+				tt.MustAssert(err != nil)
+			}
+		})
+	}
+}
+
+func TestIntList(t *testing.T) {
+	for idx, tc := range sharedIntCases {
+		t.Run(fmt.Sprintf("int/%d", idx), func(t *testing.T) {
+			tt := assert.WrapTB(t)
+			out, skip := tc.AsInts()
+			if skip {
+				t.Skip()
+			}
+
+			var v IntList
+			fs := cmdy.NewFlagSet()
+			fs.Var(&v, "s", "test")
+			err := fs.Parse(tc.in)
+
+			if tc.ok {
+				tt.MustOK(err)
+				tt.MustEqual(IntList(out), v)
+			} else {
+				tt.MustAssert(err != nil)
+			}
+		})
+	}
+}
+
+func TestFloat64List(t *testing.T) {
+	var cases = append(sharedNumCases, []numCase{
+		{yep, []string{"-s", "0.123"}, []string{"0.123"}},
+		{yep, []string{"-s", "+inf"}, []string{"+inf"}},
+		{yep, []string{"-s", "0x1.b7p-1"}, []string{"0x1.b7p-1"}},
+		{yep, []string{"-s", "0x1.b7p-1, 0x1.b7p-2"}, []string{"0x1.b7p-1", "0x1.b7p-2"}},
+	}...)
+
+	for idx, tc := range cases {
+		t.Run(fmt.Sprintf("int/%d", idx), func(t *testing.T) {
+			tt := assert.WrapTB(t)
+			out, skip := tc.AsFloat64s()
+			if skip {
+				t.Skip()
+			}
+
+			var v Float64List
+			fs := cmdy.NewFlagSet()
+			fs.Var(&v, "s", "test")
+			err := fs.Parse(tc.in)
+
+			if tc.ok {
+				tt.MustOK(err)
+				tt.MustEqual(Float64List(out), v)
+			} else {
+				tt.MustAssert(err != nil)
+			}
+		})
+	}
+}
